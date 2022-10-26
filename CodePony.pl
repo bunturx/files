67a5cac3843d05623200c4bf5a6c61689cc89c36
Moises Ruiz Diaz <ruizdiazm@gmail.com>
2022-10-26 14:49:32 -0300

    Add files via upload

---
+++ CodePony.pl
@@ -0,0 +1,53 @@
+use Acme::Pony;
+                          b                                                               
+                       U  fF                                                              
+                        ybuff                                                             
+                      ybuFFYbuFfy                                                         
+                     BUFfYbuFfYBufFY                                                      
+                     BuFFybuFfYBUffybuf                                                   
+                     FybuFfybUffybUfFYbu                                                  
+                    ffybuFfybuFfyBUfFybUf                                                 
+                  FYbUFFybUFfYbuFfYBuffYbUF                                               
+                 FybuffyBuffybuFfYbUffybUFf                                               
+                 YBufFYBuFFybUFFyBUFfYBUfFYB                                              
+                UfFYbuFfyBUFfybUffYBufFyBUFf                                              
+               ybuffyBuffYbufFybUFfYBUffyBuFf                                             
+              ybuFfYBuFFyBufFYBUffybufFybufFy                                             
+               bUffyBUFfYBuFfYbUFFyBuFFyBUfFy                                             
+                  BufFYbuFfyBUFfYBUFfYBuffybu                                             
+                     FfyBuFFYBuffybufFybufFYB                                             
+                     UffYBufFyBuffYbUfFybuFfy                           bU                
+                    fFybUffYbuFFYbuFfyBUFFYbuF                         FyBUF              
+                   fybUfFybuffybufFybuFfYbuffYb                      uFfYBufFY            
+                  buFFybuffyBuffybUfFybufFybUffyB                    UFfYBuFfYbU          
+                 FFyBuFFyBUfFyBufFYbuFfyBUFfYBUFfYBuFfYBUFfybUFFYB  UffybufFybUFF         
+        YbUff    YbufFYbuFfyBUFfybufFYBufFybuFFybUFFYbUffYbuFfYbuFFyBUFFybUfFybuf         
+      fybufFybuf fybUffybuffYbuffybuFfybuffyBuffybufFybuffybUffybuffYbuffybUFFybU         
+     ffYBUfFybUfFYbuFFYbUFfybUfFYBuffybuFfybUffyBuffYbuffYbUffYbUFfYbUffYBufFYBuFF        
+   ybuffyB  uffybUffYbUfFybUFfYBuffYBuffyBuFFyBUFFyBUffyBUfFybuffybUffYbUffYBuFFybU       
+  FFybuFf     YBUfFybufFYbuffybUffYbUffYBuFFybUFFybuffyBuffyBuFFYbUfFyBUFfybuffYbufF      
+ YbuFFy         buFFyBUfFybufFYbUFFybUFfyBuFFYbuffybuFfybUfFYbUffYbuFFybuffYBUFfyBUf      
+   FYBuFf         yBUfFyBuFFyBUffYBuffYbUfFyBUFfybUffYBuffyBufFybuFfYBUfFybUffYbUffYB     
+    uFfYBUfF         yBufFYBufFybUffYBUffybUFfYbUffyBUffybUFfYBufFYbUFFybuFfyBUffybUFf    
+      YbuFfY            buFfyBUffYBuFfybUfFyBUffYBuffYBuFfYbuffyBUffYbuffYBuFFYbUfFybUF   
+        fYbU                ffYBufFYbufFyBuFfYBUfFyBufFYbufFyBufFYBUfFybufFYbUFfyBUffyb   
+                              ufFYbuFfY        BUFfybuffyBuffyBUFfYbuFFYbUFfyBuffYbufFy   
+                              BuffyBu                   FFYBUfFyBufFYbufFYbUFfYBufFYBuF   
+                              fYbuFFy                    BUfFYBUffYbUffybu ffyBUFfyBufFY  
+                              BuFfyBu                      FFybUFFyBUffyBu FFYbuffybUffy  
+                              BuffYbu                        FfYbufFybUff  YbUffYbuFfYbU  
+                              FFYbuFf                           yBufFyBufF  yBUFfybuffyB  
+                              uffybu                            FfYbUffybu  FfYBufFyBuFF  
+                              ybUfFy                           BUFfYbUfFYBU  ffYbuFfyBuF  
+                              fybUff                         yBu     ffybuF  fyBufFybUff  
+                               YBUff                        YbUF     fybuFf   YBUFfyBuf   
+                               FybUf                       FybUff    YbUfF     yBuffYb    
+                               UFfyb                        uFf       ybUf      FyBuFf    
+                               ybuf                                   Fybu      FFYbU     
+                             ffYBUf                                   FYbu                
+                           FfybUf                                     fYBu                
+                         FFYbufF                                    yBuf                  
+                          fyBuF                                    FYBU                   
+
+
+ffYbUffybuffybUffyBuFfybufFyBuffyBUFFYbUfFYBUFfYbUfFybuFfyBUFfyBuffYbufFyBUFFYbUfFYBUFfYb
